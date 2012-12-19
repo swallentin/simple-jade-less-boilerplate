02fb9562d2521ac8fb4e40b98b38c9d7ef0c106b
swallentin <stephan.wallentin@gmail.com>
2012-12-19 12:02:51 +0100

    refactoring before implementing rest support

--- assets/js/clinics.js
+++ assets/js/clinics.js
@@ -1,20 +1,24 @@
 var data = {
-  'Offentliga kliniker': [{
-    'name': 'Sahlgrenska Universitetssjukhuset',
-    'link': 'http://www.sahlgrenska.se/SU/1/Gynekologi-och-Reproduktionsmedicin/Reproduktionsmedicin/',
-    'location': [57.682996, 11.960416]
+  "Offentliga kliniker": [{
+    "name": "Karolinska Universitetssjukhuset",
+    "link": "http://www.karolinska.se/Verksamheternas/Kliniker--enheter/Kvinnokliniken/Fertilitetsenheten/",
+    "location": [59.3511813, 18.0331045],
+    "phone": "08-585 816 15"
   }, {
-    'name': 'Karolinska Universitetssjukhuset',
-    'link': 'http://www.karolinska.se/Verksamheternas/Kliniker--enheter/Kvinnokliniken/Fertilitetsenheten/',
-    'location': [59.3511813, 18.0331045]
+    "name": "Sahlgrenska Universitetssjukhuset",
+    "link": "http://www.sahlgrenska.se/SU/1/Gynekologi-och-Reproduktionsmedicin/Reproduktionsmedicin/",
+    "location": [57.682996, 11.960416],
+    "phone": "031-342 10 00"
   }],
-  'Privata kliniker': [{
-    'name': 'Carl von Linné Kliniken',
-    'link': 'http://www.linne.se/',
-    'location': [59.8431696, 17.6385479]
+  "Privata kliniker": [{
+    "name": "Carl von Linné Kliniken",
+    "link": "http://www.linne.se/",
+    "location": [59.8431696, 17.63854],
+    "phone": "018-55 00 44"
   }, {
-    'name': 'Fertilitetscentrum Göteborg',
-    'link': 'http://www.fertilitetscentrum.se/',
-    'location': [57.70887, 11.97456]
+    "name": "Fertilitetscentrum Göteborg",
+    "link": "http://www.fertilitetscentrum.se/",
+    "location": [57.70887, 11.97456],
+    "phone": "031-710 46 00"
   }]
 };
--- assets/js/jquery-gmaps.js
+++ assets/js/jquery-gmaps.js
@@ -1,24 +1,14 @@
 (function($) {
 
+  function processData(data, map) {
 
-  var parseLatLng = function(input) {
-      input = input.replace('(', '').replace(')', '').split(',');
+    if(!data) return [];
 
-      input[0] = parseFloat(input[0]);
-      input[1] = parseFloat(input[1]);
+    var copy = $.extend({}, data);
 
-      return input;
-    };
-
-  function processMarkers(markers, map) {
-
-    if(!markers) return [];
-
-    var copy = $.extend({}, markers);
-
-    for(var level in markers) {
-      for(var i = 0; i < markers[level].length; i++) {
-        var details = markers[level][i];
+    for(var level in data) {
+      for(var i = 0; i < data[level].length; i++) {
+        var details = data[level][i];
         var marker = new google.maps.Marker({
           title: details.level,
           position: new google.maps.LatLng(
@@ -28,24 +18,13 @@
           flat: true
         });
 
+        copy[level][i] = $.extend(copy[level][i], marker);
 
-        var content = '<div>testing</div>';
-        var infoWindow = new google.maps.InfoWindow({
-          content: content
-        });
-
-        copy[level][i] = $.extend({
-          name: copy[level][i].name,
-          infoWindow: infoWindow
-        }, marker);
-
-        google.maps.event.addListener(copy[level][i], "click", function(e) {
-          infowindow.open(map, this);
-        });
       }
     }
     return copy;
   }
+
 
   var maps = function(options) {
 
@@ -53,39 +32,61 @@
 
         options.center = new google.maps.LatLng(options.center[0], options.center[1]);
 
-        $this = $(this);
+        var $this = $(this),
+            content = '',
+            infoWindow = new google.maps.InfoWindow({
+              content: content
+            });
 
         var map = new google.maps.Map(element, options);
-        options.markers = processMarkers(options.markers, map);
+        options.markers = processData(options.markers, map);
 
         var mgr = new MarkerManager(map, {
           trackMarkers: true,
           maxZoom: 15
         });
-        var mc = new MarkerClusterer(map, options.markers['Offentliga kliniker'], {
+
+
+        var markers = [].concat(options.markers['Offentliga kliniker'], options.markers['Privata kliniker']);
+
+        var mc = new MarkerClusterer(map, markers, {
           maxZoom: 15
         });
 
+        var clickedClinic = function(e) {
+          infoWindow.close();
+          map.setZoom(14);
+          map.setCenter(this.position);
+
+          var content = "<div class='infowindow'><div>$name</div><div>$phone</div><a href='$link'>Hemsida</a></div>"
+                        .replace("$name", this.name)
+                        .replace("$phone", this.phone)
+                        .replace("$link", this.link);
+
+          infoWindow.setContent(content);
+          infoWindow.open(map, this);
+          e.preventDefault();
+        };
+
         google.maps.event.addListenerOnce(map, 'idle', function() {
           var html = $("<div/>", {
-            class: 'clinics'
-          }),
-              ul = $('<ul/>').appendTo(html),
-              clinics = options.markers['Offentliga kliniker'];
+                      'class': 'clinics'
+                  });
 
+          for(var level in options.markers) {
 
-          var test = function(e) {
-            this.infoWindow.open(map, this);
-            map.setCenter(this.position)
-            e.preventDefault();
-          };
+            $('<h2/>').text(level).appendTo(html);
+            var ul = $('<ul/>').appendTo(html);
 
-          for(var i = clinics.length - 1; i >= 0; i--) {
-            var li = $('<li/>').appendTo(ul);
-            $("<a/>").attr('href', '#')
-                     .text(clinics[i].name)
-                     .attr('data-latlng', clinics[i].position.toString())
-                     .click($.proxy(test, clinics[i])).appendTo(li);
+            for(var i = options.markers[level].length - 1; i >= 0; i--) {
+              var li = $('<li/>').appendTo(ul);
+              $("<a/>").attr('href', '#')
+                       .text(options.markers[level][i].name)
+                       .click($.proxy(clickedClinic, options.markers[level][i]))
+                       .appendTo(li);
+
+              google.maps.event.addListener(options.markers[level][i], "click", $.proxy(clickedClinic, options.markers[level][i]));
+            }
           }
 
           $(".map").append(html);
